c13a6cfb40dad94da6ad9498ca6335ff3bdd20bc
naim <thanvirdx919@gmail.com>
2017-08-20 23:36:40 -0400

    second commit after typing test game is running

--- typing_test.js
+++ typing_test.js
@@ -3,22 +3,40 @@ const typingArea = document.querySelector("#typing-area");
 const clock = document.querySelector("#clock");
 const resetButton = document.querySelector("#reset");
 
-var timer = [0,0,0,0]
+var timer = [0,0,0,0];
+var interval;
+var timerRunning = false;
 
 // spelling check of typed text with the originText
 function spellCheck() {
 	let textEntered = typingArea.value;
-	console.log(textEntered);
+	let originTextMatch = originText.substring(0, textEntered.length);
+
+	if (textEntered == originText) {
+		clearInterval(interval);
+		typingArea.style.borderColor = "#00FA21";
+	}
+	else {
+		if(textEntered == originTextMatch){
+			typingArea.style.borderColor = "blue";
+		}
+		else {
+			typingArea.style.borderColor = "red";
+		}
+	}
 }
 
 //helper function to show proper timing for aesthetic reason
-function timer(time) {
-
+function helperTimer(time) {
+	if(time <= 9) {
+		time = "0" + time;
+	}
+	return time;
 }
 
 // clock math to show prper minute, second and thousand of a second
 function runTimer(){
-	let currentTime = timer[0] + ":" + timer[1] + ":" + timer[2];
+	let currentTime = helperTimer(timer[0]) + ":" + helperTimer(timer[1]) + ":" + helperTimer(timer[2]);
 	clock.innerHTML = currentTime;
 	timer[3]++;
 
@@ -31,14 +49,22 @@ function runTimer(){
 function startTimer(){
 	let textEntered = typingArea.value.length;
 	
-	if(textEntered === 0) {
-		setInterval(runTimer, 10);
+	if(textEntered === 0 && !timerRunning) {
+		timerRunning = true;
+		interval = setInterval(runTimer, 10);
 	}
 }
 
 // resetButton resets the time
 function reset() {
-	console.log("reset button has been pressed")
+	clearInterval(interval);
+	interval = null;
+	timer = [0,0,0,0];
+	timerRunning = false;
+
+	typingArea.value = "";
+	clock.innerHTML = "00:00:00";
+	typingArea.style.borderColor = "gray";
 }
 
 // event listeners
